c6c0416000ffdb73c5298cf15278b8f59c54779b
ZormistiZ <fernandroux2003@gmail.com>
2022-02-20 14:59:40 +0100

    mon seizieme commit

--- scriptjava.js
+++ scriptjava.js
@@ -1,19 +1,12 @@
-console.log("DEBUT scriptjava.js");
-
-let td = document.getElementById("HG")
-console.log(td);
-console.log("FIN scriptjava.js");
-
-
-const statusDisplay = document.querySelector('.status');
+const statusDisplay = document.querySelector('.game--status');
 
 let gameActive = true;
 let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
-const winningMessage = () => `Le joueur ${currentPlayer} a gagner!`;
+const winningMessage = () => `Joueur ${currentPlayer} à gagner!`;
 const drawMessage = () => `Le jeu ce termine en égalité!`;
-const currentPlayerTurn = () => `A '${currentPlayer}' de jouer`;
+const currentPlayerTurn = () => `A ${currentPlayer} de jouer`;
 
 statusDisplay.innerHTML = currentPlayerTurn();
 
@@ -27,3 +20,70 @@ const winningConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+ 
